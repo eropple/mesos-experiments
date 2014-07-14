b18907a8c65e1e6dda8aa0e9d7c982c37d679cb0
Ed Ropple <ed@edropple.com>
2014-07-14 02:13:15 -0400

    stupid Ed did not read the docs and now he is paying for it

--- chef/cookbooks/edcanhack_mesos/recipes/mesos_master.rb
+++ chef/cookbooks/edcanhack_mesos/recipes/mesos_master.rb
@@ -2,6 +2,10 @@ include_recipe "edcanhack_mesos::mesos_base"
 
 package "zookeeperd"
 
+bash "setting mesos quorum" do
+  code "echo '#{(node['masters'].length / 2.0).ceil}' > /etc/mesos-master/quorum"
+end
+
 bash "disabling mesos-slave job" do
   code "echo 'manual' > /etc/init/mesos-slave.override"
 end
@@ -9,4 +13,7 @@ end
 template "Zookeeper configs for HA" do
   source "zoo.cfg.erb"
   path "/etc/zookeeper/conf/zoo.cfg"
+end
+bash "seeding Zookeeper myid file" do
+  code "ruby -e 'puts `hostname`.split("-")[1].to_i + 1' > /var/lib/zookeeper/myid"
 end
